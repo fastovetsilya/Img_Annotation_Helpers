4adc664d728280bc91ff3c91c6484e057a5f2d51
Ilya <ilya.fastovets@gmail.com>
2020-09-21 12:16:33 -0700

    Initialize yolo rbbx to labelme converter

---
+++ yolo_rbbx_to_labelme.py
@@ -0,0 +1,51 @@
+"""
+Created on Mon Sep 21 12:13:11 2020
+
+@author: fastovetsilya
+
+The procedure to transform rectangular bounding boxes in YOLO 
+format from LabelImg to rectangular bounding boxes in .json 
+format for Labelme
+"""
+import json
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
